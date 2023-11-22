39742f8284f2d158ef426d3f1bc2869b5610665c
Mr. Programmer <101489543+01MrProgrammer@users.noreply.github.com>
2023-11-22 17:40:08 +0530

    Ex
    Ex

--- OS/Slips 1/Slip 16/Q2.c
+++ OS/Slips 1/Slip 16/Q2.c
@@ -1,162 +1,162 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-typedef struct {
-    int atime;
-    int cpub;
-    int priority; // Added priority attribute
-} proc_info;
-
-typedef struct rqnode {
-    int pid, priority;
-    struct rqnode *link;
-} rqnode;
-
-typedef struct {
-    int stime;
-    int pid;
-    int etime;
-} ganttchart;
-
-int n, ctime, gi = 0, wtime[20], ttime[20];
-proc_info proc[20];
-rqnode *first = NULL;
-ganttchart gchart[20];
-
-void getprocs();
-void start();
-void addprocq();
-void attachtoq(int);
-int finished();
-int gethighestpriorityproc();
-void addgchart(int);
-void dispgchart();
-void disptimes();
-
-int main(void) {
-    getprocs();
-    ctime = 0;
-    start();
-    dispgchart();
-    disptimes();
-    return 0;
+#include<string.h>
+struct input
+{
+	char pname[10];
+	int bt, at, tbt, ft, p;
+} tab[10];
+struct gantt
+{
+	char pname[10];
+	int start, end;
+} g[30], g1[30];
+int n, i, time, prev, k;
+void getinput()
+{
+	printf("\nEnter No of Processes: ");
+	scanf("%d", &n);
+	for (i = 0; i < n; i++)
+	{
+		printf("\nEnter Process Name: ");
+		scanf("%s", tab[i].pname);
+		printf("Arrival Time:");
+		scanf("%d", &tab[i].at);
+		printf("Burst Time: ");
+		scanf("%d", &tab[i].bt);
+		tab[i].tbt = tab[i].bt;
+		printf("Enter the Priority:");
+		scanf("%d", &tab[i].p);
+	}
 }
+void printinput()
+{
 
-void getprocs() {
-    printf("\nHow many processes: ");
-    scanf("%d", &n);
-    printf("\nPID\tATIME\tCPUB\tPRIORITY\n");
-    for (int i = 1; i <= n; i++) {
-        printf("%d\t", i);
-        scanf("%d\t%d\t%d", &proc[i].atime, &proc[i].cpub, &proc[i].priority);
-    }
+	printf("\nPname\tAT\tBT\tpriority");
+	for (i = 0; i < n; i++)
+	printf("\n%s\t%d\t%d\t%d", tab[i].pname, tab[i].at, tab[i].bt,tab[i].p);
 }
-
-void start() {
-    int pid;
-    while (!finished()) {
-        addprocq();
-        pid = gethighestpriorityproc();
-        if (pid != -1) {
-            ctime += proc[pid].cpub;  // Increase ctime by burst time for NP
-            proc[pid].cpub = 0;
-            printf("\nProc %d completed at time %d..", pid, ctime);
-            ttime[pid] = ctime - proc[pid].atime;
-            addgchart(pid);
-        } else {
-            ctime++;
-        }
-    }
+void printoutput()
+{
+	int TWT = 0, TTAT = 0;
+	float ATAT, AWT;
+	printf("\nPname\tAT\tBT\tpriority\tFT\tWT\tTAT");
+	for (i = 0; i < n; i++)
+	{
+		printf("\n%s\t%d\t%d\t%d\t\t%d\t%d\t%d", tab[i].pname, tab[i].at, tab[i].bt,
+		tab[i].p,tab[i].ft, tab[i].ft-tab[i].at - tab[i].bt, tab[i].ft - tab[i].at);
+		TWT = TWT + (tab[i].ft - tab[i].at - tab[i].bt);
+		TTAT = TTAT + (tab[i].ft - tab[i].at);
+	}
+	printf("\nTotal WT: %d", TWT);
+	printf("\nTotal TAT:%d", TTAT);
+	AWT = (float)TWT / n;
+	ATAT = (float)TTAT / n;
+	printf("\nAverage WT: %f", AWT);
+	printf("\nAverage TAT:%f", ATAT);
 }
-
-void addprocq() {
-    for (int i = 1; i <= n; i++) {
-        if (proc[i].atime == ctime && proc[i].cpub > 0) {
-            attachtoq(i);
-        }
-    }
+void sort()
+{
+	int pass;
+	struct input temp;
+	for (pass = 1; pass < n; pass++)
+	{
+		for (i = 0; i < n - pass; i++)
+		{
+			if (tab[i].at > tab[i + 1].at)
+			{
+				temp = tab[i];
+				tab[i] = tab[i + 1];
+				tab[i + 1] = temp;
+			}
+		}
+	}
 }
-
-void attachtoq(int pid) {
-    rqnode *curr = (rqnode *)malloc(sizeof(rqnode));
-    curr->pid = pid;
-    curr->priority = proc[pid].priority;
-    curr->link = NULL;
-
-    if (!first || first->priority > curr->priority) {
-        curr->link = first;
-        first = curr;
-    } else {
-        rqnode *temp = first;
-        while (temp->link && temp->link->priority <= curr->priority) {
-            temp = temp->link;
-        }
-        curr->link = temp->link;
-        temp->link = curr;
-    }
+int arrived(int time)
+{
+	for (i = 0; i < n; i++)
+	{
+		if (tab[i].at <= time && tab[i].tbt != 0)
+		return 1;
+	}
+	return 0;
 }
+int gethighpriority(int time)
+{
+	int processpos, min = 99;
+	for (i = 0; i < n; i++)
+	{
 
-int finished() {
-    for (int i = 1; i <= n; i++) {
-        if (proc[i].cpub != 0) {
-            return 0;
-        }
-    }
-    return 1;
+		if (tab[i].at <= time && tab[i].tbt != 0 && tab[i].p < min)
+		{
+			min = tab[i].p;
+			processpos = i;
+		}
+	}
+	return processpos;
 }
+void processinput()
+{
+	int finish = 0, j;
+	k = 0;
+	while (finish != n)
+	{
+		if (arrived(time))
+		{
+			i = gethighpriority(time);
+			for (j = 0; j < tab[i].bt; j++)
+			{
+				time++;
+				tab[i].tbt--;
+				g[k].start = prev;
+				g[k].end = time;
+				prev = time;
+				strcpy(g[k++].pname, tab[i].pname);
+				tab[i].ft = time;
+				if (tab[i].tbt == 0)
+				{
+					finish++;
+					break;
+				}
+			}
+		}
+		else
+		{
+			time++;
+			g[k].start = prev;
+			g[k].end = time;
+			strcpy(g[k++].pname, "idle");
+			prev = time;
+		}
 
-int gethighestpriorityproc() {
-    if (!first) {
-        return -1;
-    }
-    int pid = first->pid;
-    rqnode *temp = first;
-    first = first->link;
-    free(temp);
-    wtime[pid] = ctime - proc[pid].atime;
-    return pid;
+	}
 }
-
-void addgchart(int pid) {
-    gchart[++gi].pid = pid;
-    gchart[gi].stime = ctime - proc[pid].cpub;  // Adjusted for NP
-    gchart[gi].etime = ctime;
+void ganttchart()
+{
+	int i, j = 0;
+	printf("\ngantt chart is:\n");
+	g1[0] = g[0];
+	for (i = 1; i < k; i++)
+	{
+		if (strcmp(g[i].pname, g1[j].pname) == 0)
+			g1[j].end = g[i].end;
+		else
+		{
+			j++;
+			g1[j] = g[i];
+		}
+	}
+	printf("\nStart\tpname\tEnd");
+	for (i = 0; i <= j; i++)
+	{
+		printf("\n%d\t%s\t%d", g1[i].start, g1[i].pname, g1[i].end);
+	}
 }
-
-void dispgchart() {
-    printf("\n");
-    for (int i = 1; i <= gi; i++) {
-        printf("|----");
-    }
-    printf("|\n");
-    for (int i = 1; i <= gi; i++) {
-        printf("| %d ", gchart[i].pid);
-    }
-    printf("|\n");
-    for (int i = 1; i <= gi; i++) {
-        printf("|----");
-    }
-    printf("|\n");
-    for (int i = 1; i <= gi; i++) {
-        printf("%d ", gchart[i].stime);
-    }
-    printf("%d\n", gchart[gi].etime);
-}
-
-void disptimes() {
-    int sumwt = 0, sumtt = 0;
-    printf("\n**Waiting Time**");
-    printf("\nPID\tWtime");
-    for (int i = 1; i <= n; i++) {
-        printf("\n%d\t%d", i, wtime[i]);
-        sumwt += wtime[i];
-    }
-    printf("\nAverage:%.2f\n", (float)sumwt / n);
-    printf("\n**Turnaround Time**");
-    printf("\nPID\t ttime");
-    for (int i = 1; i <= n; i++) {
-        printf("\n%d\t%d", i, ttime[i]);
-        sumtt += ttime[i];
-    }
-    printf("\nAverage:%.2f\n", (float)sumtt / n);
+int main()
+{
+	getinput();
+	printinput();
+	sort();
+	processinput();
+	printoutput();
+	ganttchart();
 }
