3d21d019a0540c1520417ab96227879fdb593c06
Mr. Programmer <101489543+01MrProgrammer@users.noreply.github.com>
2023-11-25 17:33:08 +0530

    WT
    WT

--- WT & FDS/Slip 1/Q2.py
+++ WT & FDS/Slip 1/Q2.py
@@ -1,14 +1,17 @@
 #Q.2 A) Write a Python program to create a Pie plot to get the frequency of the three species of 
 #the Iris data (Use iris.csv)
 
+#S1Q1
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+iris_data = pd.read_csv(r"Iris.csv")
+
+species_counts = iris_data['Species'].value_counts()
 
 plt.figure(figsize=(8, 8))
 plt.pie(species_counts, labels=species_counts.index, autopct='%1.1f%%', startangle=140)
 plt.title('Frequency of Iris Species (Pie Chart)')
 plt.axis('equal')
-
 plt.show()
--- WT & FDS/Slip 1/Q3.py
+++ WT & FDS/Slip 1/Q3.py
@@ -1,32 +1,8 @@
+#S1Q2
 import pandas as pd
-import numpy as np
-from sklearn import preprocessing
-import scipy.stats as s
-df=pd.read_csv(r"winequality-red.csv")
-df
 
-import scipy.stats as s
-print("min max scale data")
-ds=preprocessing.MinMaxScaler(feature_range=(0,1))
-ds1=ds.fit_transform(df)
-print(ds1.round(2))
-print("---------------------------------------------")
-print("normalizded data")
-dn=preprocessing.normalize(df,norm='l1')
-print(dn.round(2))
-print("---------------------------------------------")
-print("binarized data")
-db=preprocessing.Binarizer(threshold=5).transform(df)
-print(db)
-print("---------------------------------------------")
-print("standardized data")
-print("initial mean:",s.tmean(df).round(2))
-print("initial sd:",round(df.std(),2))
-
-
-x_s=preprocessing.scale(df)
-x_s.mean(axis=0)
-x_s.std(axis=0)
-print("sd data:")
-print("scaled mean:",s.tmean(x_s).round(2))
-print("scaled sd:",round(x_s.std(),2))
+path = r'Height.csv'
+data = pd.read_csv(path)
+df = pd.DataFrame(data)
+print(df)
+print(df.describe())
