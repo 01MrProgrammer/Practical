17b63cdfb770903cfe70e0f2093823f7d806cde4
Mr. Programmer <101489543+01MrProgrammer@users.noreply.github.com>
2023-11-30 08:53:12 +0530

    JAVA
    Java

--- JAVA/Pra/Main.java
+++ JAVA/Pra/Main.java
@@ -1,26 +1,13 @@
-import java.util.Date;
-import java.text.SimpleDateFormat;
-class Main{
-    public static void main(String args[])
+import java.util.Scanner;
+
+abstract class staff{
+    protected int id;
+    protected String name;
+    
+    public staff(int id, String name)
     {
-        SimpleDateFormat sd=new SimpleDateFormat("dd/mm/yyyy");
-        String d1=sd.format(new Date());
-        System.out.println("Date:"+d1);
-
-        sd=new SimpleDateFormat("dd-mm-yyyy");
-        d1= sd.format(new Date());
-        System.out.println("Date:"+d1);
-
-        sd=new SimpleDateFormat("EEEEEEEEE MMMMM dd yyyy");
-        d1=sd.format(new Date());
-        System.out.println("Date:"+d1);
-
-        sd= new SimpleDateFormat("EEE MMMMM dd HH:mm:ss z yyyy");
-        d1=sd.format(new Date());
-        System.out.println("Date:"+d1);
-
-        sd= new SimpleDateFormat("dd/mm/yy HH:mm:ss a SSSZ");
-        d1=sd.format(new Date());
-        System.out.println("Date:"+d1);
+        this.id=id;
+        this.name=name;
     }
+    abstract void displayDetails();
 }
